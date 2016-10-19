31bcf094a65b70aa56b570d5a3ee7ee2f07dc9d2
赵越 <hzzhaoyue1@corp.netease.com>
2016-10-19 17:57:35 +0800

    code

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+
+    entry: {
+        app: './index'
+    },
+
+    output: {
+        path: './build',
+        filename: 'index.js'
+    },
+
+    devtol: 'cheap-module-eval-source-map',
+
+    devServer: {
+        inline: true,
+        port: 7777
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: "babel",
+            query: {
+                presets: ["es2015", "react"]
+            }
+        },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', '!css-loader!postcss-loader'),
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style!css'),
+            },
+            {
+                test: /\.less$/,
+
+                loader: ExtractTextPlugin.extract('style-loader','css-loader!less-loader')
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('index.css'),
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    ],
+
+    lessPlugins: [
+        new LessPluginAutoPrefix({browsers: ['last 3 version']})
+    ]
+
+};
